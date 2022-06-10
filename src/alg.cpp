65bbbc9dcabcde61c0f89c7cb3da8dff13e04679
nermolaev900 <85300440+nermolaev900@users.noreply.github.com>
2022-06-10 16:00:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,42 +1,44 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-   int p = 0;
-   for (int i = 0; i < len; i++) {
-     for (int j = i + 1; j < len; j++) {
-       if (arr[i] + arr[j] == value)
-         p += 1;
-     }
-   }
-   return p;
- }
+  int p = 0;
+    for (int i = 0; i < len; i++) {
+      for (int j = i + 1; j < len; j++) {
+        if (arr[i] + arr[j] == value)
+          p += 1;
+      }
+    }
+  return p;
+}
 int countPairs2(int *arr, int len, int value) {
-   int p = 0;
-   int t;
-   for (int i = len - 1; i >= 0; i--) {
-     if (arr[i] > value) {
-       continue;
-     } else {
-       t = i;
-       break;
-     }
+  int p = 0;
+  int t;
+  for (int i = len - 1; i >= 0; i--) {
+    if (arr[i] > value) {
+      continue;
+      } 
+      else {
+        t = i;
+        break;
+      }
    }
    for (int i = 0; i < t - 1; i++) {
      for (int j = t; j >= i + 1; j--) {
        if (arr[i] + arr[j] == value)
          p++;
      }
-   }
-   return p;
- }
+  }
+  return p;
+}
+
 int countPairs3(int *arr, int len, int value) {
-   int p = 0, t = 0, f = len - 1;
-   while (t < f - 1) {
-     int d_1 = (t + f) / 2;
-     if (arr[d_1] <= value)
-       t = d_1;
-     else
-       f = d_1;
-   }
+  int p = 0, t = 0, f = len - 1;
+  while (t < f - 1) {
+    int d_1 = (t + f) / 2;
+    if (arr[d_1] <= value)
+      t = d_1;
+    else
+      f = d_1;
+  }
    len = f - 1;
    for (int i = 0; i < len; i++) {
      t = i + 1, f = len - 1;
@@ -55,4 +57,4 @@ int countPairs3(int *arr, int len, int value) {
      p += q;
    }
    return p;
- }
+}
